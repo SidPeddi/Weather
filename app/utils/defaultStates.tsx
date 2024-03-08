489bd31d6cc4ea00b1944f637cc3878f8bbc7a52
Siddarth Peddi <siddarth4002@gmail.com>
2024-03-07 23:43:26 -0500

    test

---
+++ app/utils/defaultStates.tsx
@@ -0,0 +1,39 @@
+const defaultStates = [
+  {
+    name: "New York",
+    country: "US",
+    state: "New York",
+    lat: 40.7128,
+    lon: -74.006,
+  },
+  {
+    name: "Chicago",
+    country: "US",
+    state: "Illinois",
+    lat: 41.8781,
+    lon: -87.6298,
+  },
+  {
+    name: "Monmouth Junction",
+    country: "US",
+    state: "New Jersey",
+    lat: 40.3807,
+    lon: -74.5317,
+  },
+  {
+    name: "Terre Haute",
+    country: "US",
+    state: "Indiana",
+    lat: 39.4667,
+    lon: -87.4139,
+  },
+  {
+    name: "Bengaluru",
+    country: "IN",
+    state: "Karnataka",
+    lat: 12.9716,
+    lon: 77.5946,
+  },
+];
+
+export default defaultStates;
